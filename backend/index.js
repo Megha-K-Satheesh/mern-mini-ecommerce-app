741121e879531e5908c684653eabefdfa51cc368
Megha-K-Satheesh <megha.k.satheesh@gmail.com>
2025-11-26 20:13:57 +0530

    multer path change

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors("*"));
 
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('public/uploads'));
 
 app.use('/api', productRouters)
 
