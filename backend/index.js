07a434bf8464b23a77ada25f0ef465e1e6e7b38e
Megha-K-Satheesh <megha.k.satheesh@gmail.com>
2025-11-28 20:16:19 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors("*"));
+app.use(cors());
 
 //.use('/api/upload', express.static('public/uploads'));
 
