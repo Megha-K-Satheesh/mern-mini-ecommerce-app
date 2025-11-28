f1f11795e193fcfdd381e228fea5587f507ac746
Megha-K-Satheesh <megha.k.satheesh@gmail.com>
2025-11-28 14:11:59 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors("*"));
-
+//app.use(cors("*"));
+app.use(cors({
+    origin: 'https://mern-mini-ecommerce-app-frontend-qqmcvluc5-megha-k-ss-projects.vercel.app',
+    credentials: true
+}));
 //.use('/api/upload', express.static('public/uploads'));
 
 app.get("/", (req, res) => {
