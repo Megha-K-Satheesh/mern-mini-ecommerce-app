4062699cbd5cb6e30d15d7fdf0cf0adb985c4611
Megha-K-Satheesh <megha.k.satheesh@gmail.com>
2025-11-28 15:12:32 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //app.use(cors("*"));
 app.use(cors({
-  origin: [
-    "http://localhost:5173"
-    // "https://mern-mini-ecommerce-app-frontend-qqmcvluc5-megha-k-ss-projects.vercel.app"
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  origin:"http://localhost:5173",methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 //.use('/api/upload', express.static('public/uploads'));
